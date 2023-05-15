d47b47608121bd995baca5a904a3bb302c720d04
karol <karol05ks@gmail.com>
2023-05-15 16:03:37 +0200

    -security configuration (for dev only) { -security configuration -password encoder -user permissions -user roles }

---
+++ src/main/java/com/TaskTracker/config/security/ApplicationSecurityConfiguration.java
@@ -0,0 +1,61 @@
+package com.TaskTracker.config.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+
+import static com.TaskTracker.enums.UserRole.ADMIN;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfiguration {
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfiguration(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    protected DefaultSecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .requestMatchers("/css/**", "/images/**", "/js/**", "/register", "registration").permitAll()
+//                .requestMatchers("/api/**", "management/api/**").hasRole(ADMIN.name())
+                .requestMatchers(HttpMethod.DELETE).hasAuthority(ADMIN.name())
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+//                .formLogin(
+//                        formLogin ->
+//                                formLogin
+//                                        .loginPage("/login")
+//                                        .loginProcessingUrl("/login")
+//                                        .defaultSuccessUrl("/")
+//                                        .permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    protected UserDetailsService userDetailsService() {
+
+        UserDetails admin = User.builder()
+                .username("karol05ks@gmail.com")
+                .password(passwordEncoder.encode("32131212"))
+                .roles(ADMIN.name())
+                .build();
+
+        return new InMemoryUserDetailsManager(admin);
+    }
+}
