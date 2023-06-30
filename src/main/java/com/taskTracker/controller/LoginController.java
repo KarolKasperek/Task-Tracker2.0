0fbd39823db4846fc2cb93b359c46ed11f892bef
karol <karol05ks@gmail.com>
2023-06-30 12:44:04 +0200

    -logout system

--- src/main/java/com/taskTracker/controller/LoginController.java
+++ src/main/java/com/taskTracker/controller/LoginController.java
@@ -1,5 +1,9 @@
 package com.taskTracker.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,5 +13,14 @@ public class LoginController {
     @GetMapping("/sign-in")
     public String getLogin() {
         return "login";
+    }
+
+    @GetMapping("/sign-out")
+    public String logOut(HttpServletRequest request) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, null, authentication);
+        }
+        return "redirect:/sign-in";
     }
 }
